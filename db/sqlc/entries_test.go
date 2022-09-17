e33c7b90621934785c16235c4ed974d91a208cc0
Favour Olukayode <nerosilva522@gmail.com>
2022-09-17 19:51:34 +0100

    fix: finished tests for entries and transfers

---
+++ db/sqlc/entries_test.go
@@ -0,0 +1,118 @@
+package backend_masterclass
+
+import (
+	"backend_masterclass/util"
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateEntry(t *testing.T) {
+
+	arg := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	//We used the testify library to check the test result
+	accounts, err := testQueries.CreateAccount(context.Background(), arg)
+
+	//require keyword will end the test if the assetion doesn't pass
+	//assert keyword will log the failed assertion but will still continue the test
+	assert.Nil(t, err)
+	assert.NotNil(t, accounts)
+	require.NotEmpty(t, accounts)
+
+	createEntryArg := CreateEntryParams{
+		AccountID: accounts.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), createEntryArg)
+
+	require.Empty(t, err)
+	require.NotEmpty(t, entry)
+	require.Equal(t, entry.AccountID, accounts.ID)
+}
+
+func TestGetEntry(t *testing.T) {
+
+	arg := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	//We used the testify library to check the test result
+	accounts, err := testQueries.CreateAccount(context.Background(), arg)
+
+	//require keyword will end the test if the assetion doesn't pass
+	//assert keyword will log the failed assertion but will still continue the test
+	assert.Nil(t, err)
+	assert.NotNil(t, accounts)
+	require.NotEmpty(t, accounts)
+
+	createEntryArg := CreateEntryParams{
+		AccountID: accounts.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), createEntryArg)
+
+	require.Empty(t, err)
+	require.NotEmpty(t, entry)
+	require.Equal(t, entry.AccountID, accounts.ID)
+
+	gottenEntry, err := testQueries.GetEntry(context.Background(), entry.ID)
+
+	require.Empty(t, err)
+	require.NotEmpty(t, gottenEntry)
+	require.Equal(t, entry.Amount, gottenEntry.Amount)
+	require.Equal(t, entry.AccountID, gottenEntry.AccountID)
+}
+
+func TestListEntries(t *testing.T) {
+
+	arg := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	//We used the testify library to check the test result
+	accounts, err := testQueries.CreateAccount(context.Background(), arg)
+
+	//require keyword will end the test if the assetion doesn't pass
+	//assert keyword will log the failed assertion but will still continue the test
+	assert.Nil(t, err)
+	assert.NotNil(t, accounts)
+	require.NotEmpty(t, accounts)
+
+	for i := 0; i < 10; i++ {
+		createEntryArg := CreateEntryParams{
+			AccountID: accounts.ID,
+			Amount:    util.RandomMoney(),
+		}
+
+		testQueries.CreateEntry(context.Background(), createEntryArg)
+	}
+
+	listEntryArg := ListEntriesParams{
+		AccountID: accounts.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	listEntries, err := testQueries.ListEntries(context.Background(), listEntryArg)
+	require.Empty(t, err)
+	require.NotEmpty(t, listEntries)
+	require.Len(t, listEntries, 5)
+
+	for _, entry := range listEntries {
+		require.NotEmpty(t, entry)
+	}
+
+}
---
+++ db/sqlc/transfers_test.go
@@ -0,0 +1,146 @@
+package backend_masterclass
+
+import (
+	"backend_masterclass/util"
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateTransfer(t *testing.T) {
+
+	account1Params := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account1, err := testQueries.CreateAccount(context.Background(), account1Params)
+	assert.Nil(t, err)
+	assert.NotNil(t, account1)
+	require.NotEmpty(t, account1)
+
+	account2Params := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account2, err := testQueries.CreateAccount(context.Background(), account2Params)
+	assert.Nil(t, err)
+	assert.NotNil(t, account2)
+	require.NotEmpty(t, account2)
+
+	createTransferArg := CreateTransferParams{
+		FromAccount: account1.ID,
+		ToAccount:   account2.ID,
+		Amout:       util.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), createTransferArg)
+	assert.Nil(t, err)
+	assert.NotNil(t, transfer)
+	require.NotEmpty(t, transfer)
+	require.Equal(t, transfer.FromAccount, account1.ID)
+	require.Equal(t, transfer.ToAccount, account2.ID)
+	require.Equal(t, transfer.Amout, createTransferArg.Amout)
+
+}
+
+func TestGetTransfers(t *testing.T) {
+	account1Params := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account1, err := testQueries.CreateAccount(context.Background(), account1Params)
+	assert.Nil(t, err)
+	assert.NotNil(t, account1)
+	require.NotEmpty(t, account1)
+
+	account2Params := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account2, err := testQueries.CreateAccount(context.Background(), account2Params)
+	assert.Nil(t, err)
+	assert.NotNil(t, account2)
+	require.NotEmpty(t, account2)
+
+	for i := 0; i < 10; i++ {
+		createTransferArg := CreateTransferParams{
+			FromAccount: account1.ID,
+			ToAccount:   account2.ID,
+			Amout:       util.RandomMoney(),
+		}
+
+		testQueries.CreateTransfer(context.Background(), createTransferArg)
+	}
+	arg := GetTransfersParams{
+		FromAccount: account1.ID,
+		ToAccount:   account2.ID,
+		Limit:       5,
+		Offset:      5,
+	}
+
+	accounts, err := testQueries.GetTransfers(context.Background(), arg)
+	assert.Nil(t, err)
+	require.NotEmpty(t, accounts)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+
+}
+
+func TestGetTransfer(t *testing.T) {
+	account1Params := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account1, err := testQueries.CreateAccount(context.Background(), account1Params)
+	assert.Nil(t, err)
+	assert.NotNil(t, account1)
+	require.NotEmpty(t, account1)
+
+	account2Params := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account2, err := testQueries.CreateAccount(context.Background(), account2Params)
+	assert.Nil(t, err)
+	assert.NotNil(t, account2)
+	require.NotEmpty(t, account2)
+
+	createTransferArg := CreateTransferParams{
+		FromAccount: account1.ID,
+		ToAccount:   account2.ID,
+		Amout:       util.RandomMoney(),
+	}
+
+	createdTransfer, err := testQueries.CreateTransfer(context.Background(), createTransferArg)
+	assert.Nil(t, err)
+	assert.NotNil(t, createdTransfer)
+	require.NotEmpty(t, createdTransfer)
+	require.Equal(t, createdTransfer.FromAccount, account1.ID)
+	require.Equal(t, createdTransfer.ToAccount, account2.ID)
+	require.Equal(t, createdTransfer.Amout, createTransferArg.Amout)
+
+	gottenTransfer, err := testQueries.GetTransfer(context.Background(), createdTransfer.ID)
+	assert.Nil(t, err)
+	assert.NotNil(t, gottenTransfer)
+	require.NotEmpty(t, gottenTransfer)
+	require.Equal(t, createdTransfer.FromAccount, gottenTransfer.FromAccount)
+	require.Equal(t, createdTransfer.ToAccount, gottenTransfer.ToAccount)
+	require.Equal(t, createdTransfer.Amout, gottenTransfer.Amout)
+}
